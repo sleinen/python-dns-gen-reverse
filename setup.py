56716a58ebd6632a2f78ab2c3cc4cedab90e85e3
Simon Leinen (SWITCH) <simon.leinen@switch.ch>
2021-11-30 00:33:01 +0100

    Fix module path in entrypoint

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     entry_points={
-        'console_scripts': ['gen-reverse=gen_reverse:command_line:main'],
+        'console_scripts': ['gen-reverse=gen_reverse.command_line:main'],
     }
 )
