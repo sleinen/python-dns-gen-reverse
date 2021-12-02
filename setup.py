1c02e61aa6bc868d78e25ec64f174967a3bc3f1d
Simon Leinen (SWITCH) <simon.leinen@switch.ch>
2021-12-02 07:26:12 +0100

    Fix setup so that the gen_reverse module is included

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
 setup(
+    packages=['gen_reverse'],
     entry_points={
         'console_scripts': ['gen-reverse=gen_reverse.command_line:main'],
-    }
+    },
+    install_requires=['PyYAML'],
 )
